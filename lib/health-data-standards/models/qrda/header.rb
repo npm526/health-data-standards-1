f823b76be6bf7bc41ef00f7a4d16f0b441fd618a
Christian Arias <christian.arias@qualifacts.com>
2018-07-02 11:51:41 -0500

    Support QRDA Cat III R2.1 export for CPCPLUS program

--- lib/health-data-standards/models/qrda/header.rb
+++ lib/health-data-standards/models/qrda/header.rb
@@ -7,6 +7,7 @@ module Qrda
     embeds_one :custodian
     embeds_one :legal_authenticator
     embeds_one :information_recipient
+    embeds_one :participant
     embeds_many :performers, class_name: "Provider"
 
     field  :time , type: DateTime, default: Time.now
